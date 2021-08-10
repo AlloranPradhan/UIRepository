08da192fd7b02b45909d5cfc4a5cef7e735fd448
AlloranPradhan <87431816+AlloranPradhan@users.noreply.github.com>
2021-08-10 16:12:15 +0530

    Add files via upload

---
+++ CanvasCamUI/src/app/signature/signature.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { SignaturePad } from 'angular2-signaturepad';
+
+@Component({
+  selector: 'app-signature',
+  templateUrl: './signature.component.html',
+  styleUrls: ['./signature.component.css']
+})
+export class SignatureComponent {
+  title = 'Canvas';
+  
+  signatureImg: string;
+  @ViewChild(SignaturePad)
+  signaturePad: SignaturePad;
+
+  signaturePadOptions: Object = { 
+    'minWidth': 2,
+    'canvasWidth': 700,
+    'canvasHeight': 300
+  };
+
+  constructor() { }
+
+  ngAfterViewInit() {
+    // this.signaturePad is now available
+    this.signaturePad.set('minWidth', 2); 
+    this.signaturePad.clear(); 
+  }
+
+  drawComplete() {
+    console.log(this.signaturePad.toDataURL());
+  }
+
+  drawStart() {
+    console.log('begin drawing');
+  }
+
+  clearSignature() {
+    this.signaturePad.clear();
+  }
+
+  savePad() {
+    const base64Data = this.signaturePad.toDataURL();
+    this.signatureImg = base64Data;
+  }
+
+}
