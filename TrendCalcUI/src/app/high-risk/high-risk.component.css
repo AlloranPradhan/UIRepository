5f61d4d97db59fecddca7cd719bd626530914fa8
AlloranPradhan <87431816+AlloranPradhan@users.noreply.github.com>
2021-08-10 18:41:37 +0530

    Add files via upload

---
+++ TrendCalcUI/src/app/high-risk/high-risk.component.css
@@ -0,0 +1,24 @@
+#body{
+    /* width: 60%; */
+    text-align: center;
+    width: 60%;
+    /* align-items: center;
+    align-self: center; */
+}
+#btnid{
+    text-align: left;
+}
+.card{
+    text-align: center;
+    
+}
+#headStyle{
+    color: rgb(165, 35, 35);
+    font-weight: 900;
+    font-family: Georgia, 'Times New Roman', Times, serif;
+    font-size: x-large;
+}
+#cardimage{
+    height: 350px;
+    width: 80%;
+}
